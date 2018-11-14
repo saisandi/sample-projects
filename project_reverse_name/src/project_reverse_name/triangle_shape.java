651eb847373bbb3b420b4a6a687177effc35e45b
indraneelmc <indraneel.pexabyte@gmail.com>
2018-11-14 17:56:45 +0530

    code

--- project_reverse_name/src/project_reverse_name/triangle_shape.java
+++ project_reverse_name/src/project_reverse_name/triangle_shape.java
@@ -1,9 +1,19 @@
 package project_reverse_name;
 
+import java.util.Scanner;
+
 public class triangle_shape {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("enter how many rows:");
+		Scanner sc=new Scanner(System.in);
+		int rows=sc.nextInt();
+		for(int i=1;i<=rows;i++) {
+			for(int j=0;j<i;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
 
 	}
 
